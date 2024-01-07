91aaca8b8b3ec4843893a7c620a6cee39301e479
jaeseonPark <hadueat@gmail.com>
2024-01-08 00:37:36 +0900

    #1 - data rest 테스트 작성
    꼭 필요한 테스트는 아니지만
    data rest 테스트는 인터그레이션 테스트로 접근해야 하며
    이 때문에 `@Transactional`을 걸어줘야 함

---
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -0,0 +1,80 @@
+package com.fastcampus.projectboard.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+//Controller와 상관된 애들만 로드함 data REST와 관련된 configuration
+//@WebMvcTest
+@DisplayName("Data REST - API 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+	private final MockMvc mvc;
+
+	public DataRestTest(@Autowired MockMvc mvc) {
+		this.mvc = mvc;
+	}
+
+	@DisplayName("[api] 게시글 리스트 조회")
+	@Test
+	void givenNothing_whenRequestingArticles_thenReturnArticlesJsonResponse() throws Exception {
+		// Give
+		// When & Then
+		mvc.perform(get("/api/articles"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+	}
+
+	@DisplayName("[api] 게시글 단건 조회")
+	@Test
+	void givenNothing_whenRequestingArticles_thenReturnArticleJsonResponse() throws Exception {
+		// Give
+		// When & Then
+		mvc.perform(get("/api/articles/1"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+	}
+
+	@DisplayName("[api] 게시글 -> 댓글 리스트 조회")
+	@Test
+	void givenNothing_whenRequestingArticleCommentsFromArticle_thenReturnArticleJsonResponse() throws Exception {
+		// Give
+		// When & Then
+		mvc.perform(get("/api/articles/1/articleComments"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+	}
+
+
+	@DisplayName("[api] 댓글 리스트 조회")
+	@Test
+	void givenNothing_whenRequestingArticleComments_thenReturnArticleJsonResponse() throws Exception {
+		// Give
+		// When & Then
+		mvc.perform(get("/api/articleComments"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+	}
+
+
+	@DisplayName("[api] 댓글 단건 조회")
+	@Test
+	void givenNothing_whenRequestingArticleComment_thenReturnArticleJsonResponse() throws Exception {
+		// Give
+		// When & Then
+		mvc.perform(get("/api/articleComments/1"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+	}
+}
