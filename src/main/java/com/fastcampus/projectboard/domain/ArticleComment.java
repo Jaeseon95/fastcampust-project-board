eac07dd6e5be25e78234396de45ed62a7905e66c
jaeseonPark <hadueat@gmail.com>
2024-01-07 21:17:28 +0900

    # 13 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 엔티티클래스에 들어가는 요소이고 도메인과 직접적으로 연관이 없는 요소임으로 추출이 가능하다.
    `@MappedSuperclass`
    그외  `@DateTimeFormat` 추출

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -31,7 +31,7 @@ import lombok.ToString;
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -41,20 +41,6 @@ public class ArticleComment {
 	@Setter
 	@Column(nullable = false, length = 500)
 	private String content; // 본문
-
-	// jpa auditing
-	@CreatedDate
-	@Column(nullable = false)
-	private LocalDateTime createdAt; // 생성일자
-	@CreatedBy
-	@Column(nullable = false, length = 100)
-	private String createdBy; // 생성자
-	@LastModifiedDate
-	@Column(nullable = false)
-	private LocalDateTime modifiedAt; // 수정일자
-	@LastModifiedBy
-	@Column(nullable = false, length = 100)
-	private String modifiedBy; // 수정자
 
 	protected ArticleComment() {
 	}
