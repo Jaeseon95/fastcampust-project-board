5464fd630f0daca188e1b0029f8deefc1fd734e9
jaeseonPark <hadueat@gmail.com>
2024-01-08 01:50:22 +0900

    # 1 - Querydsl 지원을 이용하여  api 검색 옵션 추가
    `QuerydslPredicateExecutor``,
    `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,10 +1,34 @@
 package com.fastcampus.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+	JpaRepository<Article, Long>
+	// 검색 기능 추가 ExactMatching
+	, QuerydslPredicateExecutor<Article>
+	, QuerydslBinderCustomizer<QArticle>
+{
+	@Override
+	default void customize(QuerydslBindings bindings, QArticle root){
+		// 특정 property에 대해서만 검색을 열기
+		bindings.excludeUnlistedProperties(true);
+		bindings.including(root.title, root.hashtag, root.content, root.createdAt, root.createdBy);
+		// bindings.bind(root.title).first(((path, value) -> path.eq(value)));
+		// bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // likie `${v}$`
+		bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like `%${v}%`
+		bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like `%${v}%`
+		bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like `%${v}%`
+		bindings.bind(root.createdAt).first(DateTimeExpression::eq); // like `%${v}%`
+		bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // like `%${v}%`
+	};
 }
