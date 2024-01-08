1eaa223b8420c82c6fa84005f6d609a5e608af3b
hadueat <hadueat@gmail.com>
2024-01-08 11:04:14 +0900

    #17 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열지 테스트로 명세를 뽑는다.

---
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,83 @@
+package com.fastcampus.projectboard.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Disabled("개발 중")
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[view] [GET] 게시글 리스트")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
+        // Given
+        // When & Then
+        mvc.perform(get("/articles"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(view().name("articles/index"))
+            // model attribute가 있나 없나 확인까지 가능
+            .andExpect(model().attributeExists("articles"));
+    }
+
+
+    @DisplayName("[view] [GET] 게시글 상세 페이지 조회")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(view().name("articles/detail"))
+            // model attribute가 있나 없나 확인까지 가능
+            .andExpect(model().attributeExists("article"))
+            .andExpect(model().attributeExists("articleComments"));
+    }
+
+    @DisplayName("[view] [GET] 게시글 검색 페이지")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(view().name("articles/search"));
+    }
+
+    @DisplayName("[view] [GET] 해시태그 검색 페이지")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtageSearchView_thenReturnArticleHashtagSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search-hashtag"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(view().name("articles/hashtag"));
+    }
+}
+
