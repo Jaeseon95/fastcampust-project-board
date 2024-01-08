6d1c98cc4ef1297ae14cd3a4e72776e6e8deaaff
hadueat <wotjs61@naver.com>
2024-01-08 17:57:18 +0900

    #17-게시판 뷰 기본 골격작성
    컨트롤러가 핸들러메소드로 테스트를 통과하도록 열어줌
    뷰 적절히 뼈대 잡음
    테스트 mediay-type에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 업데이트

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -14,18 +14,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@Disabled("개발 중")
+//@Disabled("개발 중")
 @DisplayName("View 컨트롤러 - 게시글")
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
-
     private final MockMvc mvc;
 
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-
 
     @DisplayName("[view] [GET] 게시글 리스트")
     @Test
@@ -34,7 +32,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/index"))
             // model attribute가 있나 없나 확인까지 가능
             .andExpect(model().attributeExists("articles"));
@@ -49,7 +47,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/detail"))
             // model attribute가 있나 없나 확인까지 가능
             .andExpect(model().attributeExists("article"))
@@ -58,25 +56,27 @@ class ArticleControllerTest {
 
     @DisplayName("[view] [GET] 게시글 검색 페이지")
     @Test
-    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView()
+        throws Exception {
         // Given
 
         // When & Then
         mvc.perform(get("/articles/search"))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/search"));
     }
 
     @DisplayName("[view] [GET] 해시태그 검색 페이지")
     @Test
-    public void givenNothing_whenRequestingArticleHashtageSearchView_thenReturnArticleHashtagSearchView() throws Exception {
+    public void givenNothing_whenRequestingArticleHashtageSearchView_thenReturnArticleHashtagSearchView()
+        throws Exception {
         // Given
 
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/hashtag"));
     }
 }
